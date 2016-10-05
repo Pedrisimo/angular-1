1e6f2bbaa8fbb9c3a69d6050867f8438b05acd67
Pedrisimo <Pedro@192.168.100.7>
2016-10-06 01:43:04 +0300

    Init2

--- script/add-controller.js
+++ script/add-controller.js
@@ -1,3 +1,12 @@
-/**
- * Created by Pedro on 06.10.16.
- */
+let ang = angular.module('app', []);
+ang.controller('Todo', Todo);
+ang.value('model', {
+    users: [
+        {id: '1', name: 'Pedro', phone: '+375259645656', email: 'kapowdeveloper@gmail.com'},
+        {id: '2', name: 'Boris', phone: '+375259645656', email: 'kapowdeveloper@gmail.com'},
+        {id: '3', name: 'Egor', phone: '+375259645656', email: 'kapowdeveloper@gmail.com'}
+    ]
+});
+function Todo($scope, model) {
+    $scope.todo = model;
+}
--- script/routes.js
+++ script/routes.js
@@ -9,17 +9,14 @@
         })
                 .state('list', {
             url: '/list',
-            templateUrl: 'user-list.html'
+            templateUrl: 'user-list.html',
+                    controller: function($scope) {
+                        $scope.todo = {users: [
+                            {id: '1', name: 'Pedro', phone: '+375259645656', email: 'kapowdeveloper@gmail.com'},
+                            {id: '2', name: 'Boris', phone: '+375259645656', email: 'kapowdeveloper@gmail.com'},
+                            {id: '3', name: 'Egor', phone: '+375259645656', email: 'kapowdeveloper@gmail.com'}
+                        ]}
+                    }
         });
     });
-    ang.controller('Todo', Todo);
-    function Todo($scope, model) {
-        $scope.todo = model;
-    }
-       ang.value('model', {
-        users: [
-            {id: '1', name: 'Pedro', phone: '+375259645656', email: 'kapowdeveloper@gmail.com'},
-            {id: '2', name: 'Boris', phone: '+375259645656', email: 'kapowdeveloper@gmail.com'},
-            {id: '3', name: 'Egor', phone: '+375259645656', email: 'kapowdeveloper@gmail.com'}
-        ]
-    });
+
